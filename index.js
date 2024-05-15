374ec272f9526ba8de2acbdb1401ad71e30a5ed5
Samuel Mello <6161359+samueljmello@users.noreply.github.com>
2024-05-15 10:30:12 -0400

    test contexts

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ try {
 
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const payload = JSON.stringify(github.context.secrets, undefined, 2)
+  const payload_v = JSON.stringify(github.context.variables, undefined, 2)
+  console.log(`The secrets payload: ${payload}`);
+  console.log(`The variables payload: ${payload_v}`);
 
 } catch (error) {
   core.setFailed(error.message);
