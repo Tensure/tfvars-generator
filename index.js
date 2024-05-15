95b5d10a2ee092ff78b08bcef01e0e396b68fe58
Samuel Mello <6161359+samueljmello@users.noreply.github.com>
2024-05-15 10:37:28 -0400

    updates

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const fileName = core.getInput('file-name');
-  const secretMappings = core.getInput('secret-mappings');
-  const variableMappings = core.getInput('variable-mappings');
+  const fileName = core.getInput("file-name");
+  const secretMappings = core.getInput("secret-mappings");
+  const variableMappings = core.getInput("variable-mappings");
 
   console.log(`file: ${fileName}`);
   console.log(`secret mappings: ${secretMappings}`);
   console.log(`variable mappings: ${variableMappings}`);
 
-
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.secrets, undefined, 2)
-  const payload_v = JSON.stringify(github.context.variables, undefined, 2)
+  const payload = JSON.stringify(github.context.secrets, undefined, 2);
+  const payload_v = JSON.stringify(github.context.variables, undefined, 2);
   console.log(`The secrets payload: ${payload}`);
   console.log(`The variables payload: ${payload_v}`);
-
 } catch (error) {
   core.setFailed(error.message);
 }
